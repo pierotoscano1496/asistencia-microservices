c7fbdd659934c74308fe1a0dcacf4a36a58a56d1
Piero Toscano <iepvptr@gmail.com>
2021-03-09 14:29:41 -0500

    Fixing microservices routes

--- cliente-service/src/main/java/com/piero/clienteservice/controller/ClienteController.java
+++ cliente-service/src/main/java/com/piero/clienteservice/controller/ClienteController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
-@RequestMapping("/cliente")
+@RequestMapping("cliente")
 public class ClienteController {
     @Autowired
     private ClienteService service;
